0da870e6c1aff2a98847baed7280fde90d13129e
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-13 09:24:22 -0600

    Poro: update map poro with location (city, county name) attribute

--- spec/poros/map_spec.rb
+++ spec/poros/map_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Map poro' do
   before do
     data = {
-      lat: 39.738453,
-      lng: -104.984853
+      adminArea5: 'Denver',
+      latLng: {
+        lat: 39.738453,
+        lng: -104.984853
+      }
     }
     @map = Map.new(data)
   end
@@ -13,5 +16,6 @@ RSpec.describe 'Map poro' do
     expect(@map).to be_a(Map)
     expect(@map.lat).to eq(39.738453)
     expect(@map.lng).to eq(-104.984853)
+    expect(@map.location).to eq('Denver')
   end
 end
