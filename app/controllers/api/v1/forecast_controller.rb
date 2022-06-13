0725aa7f19c0ae2df5c6e5c2bd9afb95e69ec17b
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-13 00:49:21 -0600

    Request: Backgrounds request sends image for a given area

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ForecastController < ApplicationController
   def index
     coordinates = MapFacade.get_coordinates(params[:location])
     forecast = ForecastFacade.get_forecast(coordinates.lat, coordinates.lng)
-    # binding.pry
+
     render json: ForecastSerializer.forecast(forecast)
   end
 end
