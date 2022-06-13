f0f32c2544b2d042c909f8d1b11ae8b1dd1af1a8
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-13 10:31:01 -0600

    Fix: forecast service gets temps in Farenheit

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -1,7 +1,7 @@
 class ForecastService < BaseService
   class << self
     def get_forecast(lat, lon)
-      response = conn('https://api.openweathermap.org').get("data/2.5/onecall?lat=#{lat}&lon=#{lon}&exclude=minutely,alerts&appid=#{ENV['weather_key']}")
+      response = conn('https://api.openweathermap.org').get("data/2.5/onecall?lat=#{lat}&lon=#{lon}&exclude=minutely,alerts&units=imperial&appid=#{ENV['weather_key']}")
 
       get_json(response)
     end
