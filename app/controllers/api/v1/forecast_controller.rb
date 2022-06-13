539b374bbb9c39bfe4818b3f3cb63fac63a0f3d0
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-12 22:35:25 -0600

    Request: forecast request sends forecast for a given area

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,8 @@
-class ForecastController < ApplicationController
+class Api::V1::ForecastController < ApplicationController
   def index
-
+    coordinates = MapFacade.get_coordinates(params[:location])
+    forecast = ForecastFacade.get_forecast(coordinates.lat, coordinates.lng)
+    # binding.pry
+    render json: ForecastSerializer.forecast(forecast)
   end
 end
--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,47 +1,48 @@
 class ForecastSerializer
-  include JSONAPI::Serializer
-
-  def forecast(data)
-    {
-      data: {
-        id: nil,
-        type: 'forecast',
-        attributes: {
-          current_weather: {
-            datetime: data[:current].datetime,
-            sunrise: data[:current].sunrise,
-            sunset: data[:current].sunset,
-            temperature: data[:current].temp,
-            feels_like: data[:current].feels_like,
-            humidity: data[:current].humidity,
-            uvi: data[:current].uvi,
-            visibility: data[:current].visibility,
-            conditions: data[:current].conditions,
-            icon: data[:current].icon
-          },
-          daily_weather:
-          data[:daily].map do |dw|
-            {
-              date: dw.date,
-              sunrise: dw.sunrise,
-              sunset: dw.sunset,
-              max_temp: dw.max_temp,
-              min_temp: dw.min_temp,
-              conditions: dw.conditions,
-              icon: dw.icon
-            }
-          end,
-          hourly_weather:
-          data[:hourly].map do |hw|
-            {
-              time: hw.time,
-              temperature: hw.temp,
-              conditions: hw.conditions,
-              icon: hw.icon
-            }
-          end
+  # include JSONAPI::Serializer
+  class << self
+    def forecast(data)
+      {
+        data: {
+          id: nil,
+          type: 'forecast',
+          attributes: {
+            current_weather: {
+              datetime: data.current[:datetime],
+              sunrise: data.current[:sunrise],
+              sunset: data.current[:sunset],
+              temperature: data.current[:temp],
+              feels_like: data.current[:feels_like],
+              humidity: data.current[:humidity],
+              uvi: data.current[:uvi],
+              visibility: data.current[:visibility],
+              conditions: data.current[:conditions],
+              icon: data.current[:icon]
+            },
+            daily_weather:
+            data.daily.map do |dw|
+              {
+                date: dw[:date],
+                sunrise: dw[:sunrise],
+                sunset: dw[:sunset],
+                max_temp: dw[:max_temp],
+                min_temp: dw[:min_temp],
+                conditions: dw[:conditions],
+                icon: dw[:icon]
+              }
+            end,
+            hourly_weather:
+            data.hourly.map do |hw|
+              {
+                time: hw[:time],
+                temperature: hw[:temp],
+                conditions: hw[:conditions],
+                icon: hw[:icon]
+              }
+            end
+          }
         }
       }
-    }
+    end
   end
 end
