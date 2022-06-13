5d553705dc78edc20e23db5c6515dba5bc716797
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-13 10:38:52 -0600

    Fix: add quantity param to BookService spec get_books argument

--- spec/services/book_service_spec.rb
+++ spec/services/book_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Book Service' do
   it 'gets book results for given location', :vcr do
-    response = BookService.get_books('denver,co')
+    response = BookService.get_books('denver,co', 5)
 
     expect(response).to be_a Hash
     expect(response[:docs]).to be_a(Array)
