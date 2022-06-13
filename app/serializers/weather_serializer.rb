49e577d77e5c91681bc2a459d3126dc4e3ebab4d
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-12 21:14:32 -0600

    create Weather serializer

---
+++ app/serializers/weather_serializer.rb
@@ -0,0 +1,47 @@
+class WeatherSerializer
+  include JSONAPI::Serializer
+
+  def weather(data)
+    {
+      data: {
+        id: nil,
+        type: 'forecast',
+        attributes: {
+          current_weather: {
+            datetime: data[:current].datetime,
+            sunrise: data[:current].sunrise,
+            sunset: data[:current].sunset,
+            temperature: data[:current].temp,
+            feels_like: data[:current].feels_like,
+            humidity: data[:current].humidity,
+            uvi: data[:current].uvi,
+            visibility: data[:current].visibility,
+            conditions: data[:current].conditions,
+            icon: data[:current].icon
+          },
+          daily_weather:
+          data[:daily].map do |dw|
+            {
+              date: dw.date,
+              sunrise: dw.sunrise,
+              sunset: dw.sunset,
+              max_temp: dw.max_temp,
+              min_temp: dw.min_temp,
+              conditions: dw.conditions,
+              icon: dw.icon
+            }
+          end,
+          hourly_weather:
+          data[:hourly].map do |hw|
+            {
+              time: hw.time,
+              temperature: hw.temp,
+              conditions: hw.conditions,
+              icon: hw.icon
+            }
+          end
+        }
+      }
+    }
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -70,9 +70,9 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  config.filter_sensitive_data("Sike you thought") { ENV['map_key'] }
-  config.filter_sensitive_data("Sike you thought") { ENV['map_secret'] }
-  config.filter_sensitive_data("Sike you thought") { ENV['weather_key'] }
+  # config.filter_sensitive_data("Sike you thought") { ENV['map_key'] }
+  # config.filter_sensitive_data("Sike you thought") { ENV['map_secret'] }
+  # config.filter_sensitive_data("Sike you thought") { ENV['weather_key'] }
   config.allow_http_connections_when_no_cassette = true
 end
 
