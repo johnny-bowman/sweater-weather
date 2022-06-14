afaa335466c9eda333589e449ae0fdd8bd1f3bfa
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-14 15:23:48 -0600

    Request: /api/v1/users creates user and returns user info

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,17 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
   def create
+    @user = User.create(user_params)
+    if @user.save && user_params[:password] == user_params[:password_confirmation]
+      @user.api_key = SecureRandom.urlsafe_base64
+      render json: UserSerializer.new_user(@user), status: 201
+    else
+      render json: UserSerializer.creation_error(@user), status: 400
+    end
+  end
 
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
---
+++ app/serializers/api/v1/user_serializer.rb
@@ -0,0 +1,26 @@
+class Api::V1::UserSerializer
+  class << self
+    def new_user(user)
+      {
+        data: {
+          type: 'users',
+          id: user.id,
+          attributes: {
+            email: user.email,
+            api_key: user.api_key
+          }
+        }
+      }
+    end
+
+    def creation_error(user)
+      if (user.email.nil? || user.email.empty?)
+        { error: "email can't be blank" }
+      elsif User.find_by(email: user.email)
+        { error: "that email is already taken" }
+      elsif user.password != user.password_confirmation
+        { error: "password confirmation doesn't match password" }
+      end
+    end
+  end
+end
