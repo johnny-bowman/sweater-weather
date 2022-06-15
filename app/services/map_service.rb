545df1144222d084e71444d40f60b6a4edd59729
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-14 20:42:04 -0600

    Service: Map service returns travel time between two locations

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -2,7 +2,13 @@ class MapService < BaseService
   class << self
     def get_coordinates(location)
       response = conn('http://www.mapquestapi.com').get("geocoding/v1/address?key=#{ENV['map_key']}&location=#{location}")
-      
+
+      get_json(response)
+    end
+
+    def get_route(from, to)
+      response = conn('http://www.mapquestapi.com').get("directions/v2/route?key=#{ENV['map_key']}&from=#{from}&to=#{to}")
+
       get_json(response)
     end
   end
