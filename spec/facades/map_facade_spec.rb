0bd25d19aa54dc4d3a709324c1009efeccf3b515
Johnny Bowman <johntbowman.production@gmail.com>
2022-06-14 21:28:35 -0600

    Test: Map facade creates an instance of Route poro

--- spec/facades/map_facade_spec.rb
+++ spec/facades/map_facade_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Map facade' do
-  before do
+  it 'creates an instance of Map poro', :vcr do
     @coordinates = MapFacade.get_coordinates('denver,co')
+    expect(@coordinates).to be_a(Map)
   end
 
-  it 'creates an instance of Map poro', :vcr do
-    expect(@coordinates).to be_a(Map)
+  it 'creates an instance of Route poro', :vcr do
+    @route = MapFacade.get_route('Denver,CO', 'Pueblo,CO')
+    expect(@route).to be_a(Route)
   end
 end
